cb3046595c9857d8cf7c60c6b2c238c91b86c12e
Hanyu <rollr76518@gmail.com>
2017-10-22 11:57:02 +0800

    - 新增 MapManager

---
+++ SalesTraveling/Managers/MapMananger.swift
@@ -0,0 +1,13 @@
+//
+//  MapMananger.swift
+//  SalesTraveling
+//
+//  Created by Hanyu on 2017/10/22.
+//  Copyright © 2017年 Hanyu. All rights reserved.
+//
+
+import MapKit
+
+class MapMananger {
+
+}
