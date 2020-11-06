26aa9778793f09669543a4591fb59e85ab044bfd
Hanyu <rollr76518@gmail.com>
2020-11-06 23:23:35 +0800

    Add Test Target.

---
+++ SalesTravelingTests/SalesTravelingTests.swift
@@ -0,0 +1,33 @@
+//
+//  SalesTravelingTests.swift
+//  SalesTravelingTests
+//
+//  Created by Ryan on 2020/11/6.
+//  Copyright © 2020 Hanyu. All rights reserved.
+//
+
+import XCTest
+
+class SalesTravelingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
